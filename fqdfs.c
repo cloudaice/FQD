bdc0684c01520ff079cb30043a85fbcdef27b97e
cloudaice <cloudaice@163.com>
2012-05-18 16:42:21 +0800

    建立初始文件

---
+++ fqdfs.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include <errno.h>
+#include <fuse.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+
+
