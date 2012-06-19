ce4a6aecf7f7a17186c8a158479605a75e930cfe
cloudaice <cloudaice@163.com>
2012-06-19 21:52:19 +0800

    修改了注释，合并了main.c文件

--- fqdfs.c
+++ fqdfs.c
@@ -13,13 +13,19 @@
 
     #include <log.h>
 
+   // 把错误报给日志文件
     static int fqd_error(char *str)
     {
         int ret = -errno;
         log_msg("    ERROR %s: %s\n",str,strerror(errno));
         return ret;
     }
-
+ /*
+检查授权用户是否被允许做给定的操作
+我看到的所有的路径是相对安装的根文件系统。为了得到底层的文件系统，我需要
+挂载点。我将它保存在main（），然后每当我需要寻找某东西路径，我会找到给这个挂载点去
+构造它
+*/
     static void fqd_fullpath(char fpath[PATH_MAX], const char *path)
     {
         strcpy(fpath, BB_DATA->rootdir);
@@ -28,6 +34,10 @@
     	    BB_DATA->rootdir, path, fpath);
     }
 
+/*
+ 类似于Stat()函数，'st_dev' 和 'st_blksize'文件时被忽视的， 除非'use_ino'挂载给出，
+ 否则'st_ino'是被忽略的。
+*/
     int fqd_getattr(const char *path, struct stat *statbuf)
     {
         int retstat = 0;
@@ -42,6 +52,15 @@
         return retstat;
     }
 
+/** 读一个符号链接的目标
+ *
+缓冲区应充满一个空结束的字符串。
+缓冲区的大小参数包括终止的空间空字符。
+如果linkname是太长，不匹配在缓冲区，它应该被截断。
+如果成功返回值应该是0。
+考虑到系统readlink（）系统将截断并且失去终结的空字符。因此，
+传递到readlink（）系统的大小必须小于传递到fqd_readlink（）。
+ */
     int fqd_readlink(const char *path, char *link, size_t size)
     {
     int retstat = 0;
@@ -58,7 +77,10 @@
         }
         return retstat;
     }
-
+/** 创建文件节点
+ 我们没有写create（）函数，这里的mknod（）将创建所有 non-directory和
+  non-symlink 节点。
+ */
     int fqd_mknod(const char *path, mode_t mode, dev_t dev)
     {
             int retstat = 0;
@@ -67,9 +89,6 @@
     log_msg("\nfqd_mknod(path=\"%s\", mode=0%3o, dev=%lld)\n",
       path, mode, dev);
     fqd_fullpath(fpath, path);
-    
-    // On Linux this could just be 'mknod(path, mode, rdev)' but this
-    //  is more portable
     if (S_ISREG(mode)) {
         retstat = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
     if (retstat < 0)
@@ -91,7 +110,7 @@
     }
     return retstat;
     }
-
+   /* 创建一个目录 */
     int fqd_mkdir(const char *path, mode_t mode)
     {
           int retstat = 0;
@@ -99,7 +118,7 @@
     
     log_msg("\nfqd_mkdir(path=\"%s\", mode=0%3o)\n",
         path, mode);
-    fqd_fullpath(fpath, path);
+    fqd_fullpath(fpath, path);//加载路径
     
     retstat = mkdir(fpath, mode);
     if (retstat < 0)
@@ -108,7 +127,7 @@
     return retstat;
         
     }
-
+/* 删除文件 */
     int fqd_unlink(const char *path)
     {
         int retstat = 0;
@@ -118,7 +137,7 @@
         path);
     fqd_fullpath(fpath, path);
     
-    retstat = unlink(fpath);
+    retstat = unlink(fpath);//删除链接
     if (retstat < 0)
     retstat = fqd_error("fqd_unlink unlink");
     
@@ -143,6 +162,12 @@
 
     }
 
+/*
+创建一个符号链接 
+ 这里的参数是有点混乱，但是符合symlink()系统的调用。
+ “路径”是其中的连结点， 而“链接”是链接本身。
+  因此，我们要使的路径不改变，但插入到安装目录的链接。
+ */
     int fqd_symlink(const char *path, const char *link)
     {
         int retstat = 0;
@@ -159,9 +184,10 @@
     return retstat;
     }
 
+    //重命名
     int fqd_rename(const char *path, const char *newpath)
     {
-        int retstat = 0;
+     int retstat = 0;
     char fpath[PATH_MAX];
     char fnewpath[PATH_MAX];
     
@@ -177,7 +203,7 @@
     return retstat;
 
     }
-
+ //创建一个文件的硬链接
     int fqd_link(const char *path, const char *newpath)
     {
          int retstat = 0;
@@ -191,11 +217,9 @@
     retstat = link(fpath, fnewpath);
     if (retstat < 0)
     retstat = fqd_error("fqd_link link");
-    
     return retstat;
-
     }
-
+//改变文件的quanxian
     int fqd_chmod(const char *path, mode_t mode)
     {
          int retstat = 0;
@@ -212,7 +236,7 @@
     return retstat;
 
     }
-
+//改变文件的组和所属人
     int fqd_chown(const char *path, uid_t uid, gid_t gid)
     {
         int retstat = 0;
@@ -229,7 +253,7 @@
     return retstat;
 
     }
-
+/** 改变文件大小*/
     int fqd_truncate(const char *path, off_t newsize)
     {
          int retstat = 0;
