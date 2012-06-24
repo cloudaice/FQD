2ae6b3226e1676d0d61e36b50c4710ab2a7e27d2
cloudaice <cloudaice@163.com>
2012-06-24 16:34:07 +0800

    修改了函数

--- log.h
+++ log.h
@@ -2,9 +2,8 @@
 #define _LOG_H_
 #include <stdio.h>
 
-//  macro to log fields in structs.
-#define log_struct(st, field, format, typecast) \
-  log_msg("    " #field " = " #format "\n", typecast st->field)
+//  进行log_struct宏定义
+#define log_struct(st, field, format, typecast) log_msg("    " #field " = " #format "\n", typecast st->field)
 
 FILE *log_open(void);
 void log_fi (struct fuse_file_info *fi);
